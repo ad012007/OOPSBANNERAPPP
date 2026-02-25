54f2b3ffaa7e13739c8793d5069620ef3b9cdbd6
ad012007 <ad0120@srmist.edu.in>
2026-02-25 13:08:31 +0530

    add

---
+++ uc4 .java
@@ -0,0 +1,17 @@
+public class uc4 {
+    public static void main(String[] args) {
+
+        String[] banner = {
+            " *****    *****    ******    ***** ",
+            "*     *  *     *   *     *  *      ",
+            "*     *  *     *   ******    ***** ",
+            "*     *  *     *   *              *",
+            " *****    *****    *        ***** "
+        };
+
+        // Using loop to print the banner
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
